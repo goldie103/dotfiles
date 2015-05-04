e01872a7de5dc5b599e180babe4b828e9bbbc329
Kelly Stewart <kelly.g.stewart@education.nsw.gov.au>
2015-05-05 03:03:35 +1000

    File restructuring. Last time probably.

--- emacs/.emacs.d/flycheck_autocorrect-abbrevs.el
+++
@@ -1,8 +0,0 @@
-;;; autocorrect-abbrevs.el --- common auto correction like abbrevs
-;;; Commentary:
-;; http://emacswiki.org/emacs/autocorrection_abbrev_defs
-;;; Code:
-
-
-(provide 'autocorrect-abbrevs)
-;;; autocorrect-abbrevs.el ends here
