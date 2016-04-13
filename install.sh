5c0063d33a76c6aae42fc1274af52c03ae58d304
goldie103 <miscoined@gmail.com>
2016-04-13 11:10:16 +1000

    Rename to init.** naming scheme

--- install.sh
+++ install.sh
@@ -1,29 +1,30 @@
-#!/bin/sh
-
-source env
+#!/bin/zsh
 
 exists () { command -v $1 >/dev/null 2>&1; }
 
-[[ "$EUID" -ne 0 ]] && SUDO=true || SUDO=false
+[[ $EUID -ne 0 ]] && SUDO=true || SUDO=false
+
+source ./env
+[[ ! -L $HOME/.zshenv ]] && ln -sv "$DOT/env" "$HOME/.zshenv"
 
 # install git
-if [[ ! "$(exists git)" ]]; then
-	if ! $SUDO; echo "Please install git" && exit; fi
-	if [ ! "$(exists apt)" ]; then apt-get install git
-	elif [[ ! "$(exists pacman)" ]]; then pacman -S git
-	fi
-fi
+[[ ! $(exists git) ]] && echo "Please install git"
 
 # vim
 VIMLOC=${VIMDOTDIR:-$XDG_CONFIG_HOME/vim}
-[[ ! -d "$VIMLOC" ]] && mkdir $VIMLOC && ln -s "$DOT/init.vim" "$VIMLOC/vimrc"
+if [[ ! -d $VIMLOC ]]; then
+  echo "Setting up vim ..."
+  mkdir $VIMLOC && ln -s "$DOT/init.vim" "$VIMLOC/vimrc"
+fi
 
 # vundle
-[[ ! -d "$VIMLOC/vundle" ]] &&
-	git clone https://github.com/VundleVim/Vundle.vim.git $VIMLOC/bundle/Vundle.vim &&
-	vim +PluginInstall +qall
+if [[ ! -d $VIMLOC/vundle ]]; then
+  echo "Setting up vundle..."
+	git clone https://github.com/VundleVim/Vundle.vim.git $VIMLOC/bundle/Vundle.vim && vim +PluginInstall +qall
+fi
 
 # vimperator
-[[ ! -d "$XDG_CONFIG_HOME/vimperator/plugin" ]] &&
-	mkdir "$XDG_CONFIG_HOME/vimperator/plugin" &&
-	curl -o "$XDG_CONFIG_HOME/vimperator/plugin/smooziee.js" https://raw.githubusercontent.com/vimpr/vimperator-plugins/master/_smooziee.js
+if [[ ! -d $VIMPERATOR_RUNTIME/plugin ]]; then
+	mkdir "$VIMPERATOR_RUNTIME/plugin" &&
+	curl -o "$VIMPERATOR_RUNTIME/plugin/smooziee.js" https://raw.githubusercontent.com/vimpr/vimperator-plugins/master/_smooziee.js
+fi
